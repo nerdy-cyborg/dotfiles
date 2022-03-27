d58a77af3e4da376fd5f26504d764888c314a4b2
William Ellwood <william@grepler.io>
2022-03-27 14:16:00 -0400

    Update brew.sh
    added macfuse and tailscale

--- brew.sh
+++ brew.sh
@@ -98,11 +98,14 @@ brew install vbindiff
 brew install zopfli
 brew install md5deep
 brew install xxhash
+brew install tailscale
+brew install sizeup
+
 
 # Install casked software
 brew install --cask visual-studio-code
 brew install --cask amethyst
-brew install sizeup
+brew install --cask macfuse
 
 # Remove outdated versions from the cellar.
 brew cleanup
